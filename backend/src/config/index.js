40947545fb33d5b8ec148fc729ee0c2a083b12f4
LaXnZ <sumudithalanz@gmail.com>
2025-03-01 23:36:03 +0530

    Removed deprecated code

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected...");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
